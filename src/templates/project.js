b6ab3f48e3ca3b535fe478d59f420a82df28cf00
bnbln <30811632+bnbln@users.noreply.github.com>
2019-09-11 21:58:58 +0200

    proj page

---
+++ src/templates/project.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { kebabCase } from 'lodash'
+import Helmet from 'react-helmet'
+import { graphql, Link } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+import ReactPlayer from 'react-player'
+
+export const ProjectTemplate = ({
+  title,
+  helmet,
+}) => {
+
+  return (
+    <section className="section">
+      {helmet || ''}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+      
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+ProjectTemplate.propTypes = {
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+}
+
+const Project = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <ProjectTemplate
+        content={post.html}
+        contentComponent={HTMLContent}
+        helmet={
+          <Helmet titleTemplate="%s | Blog">
+            <title>{`${post.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${post.frontmatter.title}`}
+            />
+          </Helmet>
+        }
+
+      />
+    </Layout>
+  )
+}
+
+Project.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+}
+
+export default Project
+
+export const pageQuery = graphql`
+  query ProjectsByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
